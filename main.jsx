dd44df82e9c79e896b744496b98036b3d4236817
02Geeta <124858923+02Geeta@users.noreply.github.com>
2025-09-09 21:17:37 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import Educhat from './educhat.jsx/'
+
+createRoot(document.getElementById("root")).render(
+    <Educhat />
+
+)
